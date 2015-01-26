548a70380ce6f6124a2dbee2f1f252e1d039f72b
Long Jing <long.jing618@gmail.com>
2015-01-26 02:47:58 -0500

    add custom_libs

---
+++ custom_libs/customjs.js
@@ -0,0 +1,279 @@
+$(".accept-help").on( "click", myHandler );
+
+function myHandler( event ) 
+{
+	//alert($( this ).next().html())
+	$.ajax({
+	      url: 'node/get/ajax/'+$( this ).next().html(),
+	      type: 'post',
+	      success: function(data, status) 
+	      {
+	      		;
+	      },
+	      error: function(xhr, desc, err) {
+	      		;
+	      }
+	    });
+}
+
+basics = {};
+
+function generatepopworld()
+{
+	basics = init();
+	getOutput("freq");
+}
+
+/*
+function print()
+{
+	var ret = "";
+	$.each(basics, function(key,val){
+		ret += key+": " + val + "<br/>";
+	})
+	$("#result").html(ret);
+}
+*/
+
+function getOutput(name)
+{
+	//alert("Basics");
+	zeroGeneration();
+	count = $("#steps").val();
+	calculateNextNGenerations(count);
+	getChart(name);
+	//print();
+	//if($("#edit-next").length == 0)
+		$( '<button role="button" class="pop_button" id="next"><span class="ui-button-text">Next</span></button>' ).insertAfter( "#start" );
+		$("#start").hide();
+
+	$("#next").click(function(){
+		count = $("#steps").val();
+		var status = 0;
+		if($("#chartdiv").is(":visible"))
+			status = 1;
+
+		if(status == 0)
+			$("#chartdiv").show();
+
+		calculateNextNGenerations(count);
+		getChart($("#graph_type").val());
+		//print();
+
+		if(status == 0)
+			$("#chartdiv").hide()
+	})
+}
+
+function roundnumber(num)
+{
+	var temp = 100000;
+	num = (Math.round(num * temp)/temp).toFixed(5);
+	return num;
+}
+
+function generatecdata(name)
+{
+	cdata = [];
+	var red;
+    var blue;
+    var data;
+    for (var i = 0; i < basics.count; i++) 
+    {
+    	data = basics.data[i];
+    	if(data.dominant == "red")
+    	{
+       		//red = data.currentFrequencyA1.toFixed(2);
+       		//blue = data.currentFrequencyA2.toFixed(2);
+       		red = data.currentDominantPopCount;
+       		blue = data.currentRecessivePopCount;
+       	}
+       	else
+       	{
+       		//blue = data.currentFrequencyA1.toFixed(2);
+       		//red = data.currentFrequencyA2.toFixed(2);
+       		blue = data.currentDominantPopCount;
+       		red = data.currentRecessivePopCount;
+       	}
+
+       	switch(name)
+		{
+			case "freq":
+				cdata.push({
+		            count: i,
+		            A1: roundnumber(data.currentFrequencyA1),
+		            A2: roundnumber(data.currentFrequencyA2),
+					A1A1:roundnumber(data.currentFrequencyA1A1),
+					A1A2:roundnumber(data.currentFrequencyA1A2),
+					A2A2:roundnumber(data.currentFrequencyA2A2),
+		            //des:"123"
+	        	});
+	        	break;
+			case "pop_count":
+			case "genotypes":
+			case "phenotypes":
+			default:
+				return [];
+		}
+    }
+    return cdata;
+}
+
+/*
+data: the chart data
+title: chart title
+valueAxes
+graphs
+*/
+function getChartData(name)
+{
+	var chartdata = {};
+	var cdata = generatecdata(name);
+	var title;
+	var valueAxes;
+	var graphs;
+
+	switch(name)
+	{
+		case "freq":
+			valueAxes = [{
+					        "id":"v1",
+					        "axisColor": "#FF6600",
+					        "axisThickness": 2,
+					        "gridAlpha": 0,
+					        "axisAlpha": 1,
+					        "position": "left",
+					        "minimum":0,
+					        "maximum":1
+					    }, {
+					        "id":"v2",
+					        "axisColor": "#FCD202",
+					        "axisThickness": 2,
+					        "gridAlpha": 0,
+					        "axisAlpha": 1,
+					        "position": "right",
+							"minimum":0,
+					        "maximum":1
+					    },];
+			title = "Frequency";
+			graphs = [{
+				        "valueAxis": "a1",
+				        "lineColor": ($("#dominance").val() == "RED"? "red":"blue"),
+				        "bullet": "round",
+				        "bulletBorderThickness": 1,
+				        "hideBulletsCount": 30,
+				        "title": "Red",
+				        "valueField": "A1",
+						"fillAlphas": 0,
+				        "type": "smoothedLine",
+				        //"balloonFunction":getTitle,
+				    }, {
+				        "valueAxis": "a2",
+				        "lineColor": ($("#dominance").val() == "RED"? "blue":"red"),
+				        "bullet": "square",
+				        "bulletBorderThickness": 1,
+				        "hideBulletsCount": 30,
+				        "title": "Blue",
+				        "valueField": "A2",
+						"fillAlphas": 0,
+				        "type": "smoothedLine",
+				    }, {
+				        "valueAxis": "a1a1",
+				        "lineColor": "#009900",
+				        "bullet": "diamond",
+				        "bulletBorderThickness": 1,
+				        "hideBulletsCount": 30,
+				        "title": "A1A1",
+				        "valueField": "A1A1",
+						"fillAlphas": 0,
+				        "type": "smoothedLine",
+				    }, {
+				        "valueAxis": "a1a2",
+				        "lineColor": "#CC6600",
+				        "bullet": "triangleLeft",
+				        "bulletBorderThickness": 1,
+				        "hideBulletsCount": 30,
+				        "title": "A1A2",
+				        "valueField": "A1A2",
+						"fillAlphas": 0,
+				        "type": "smoothedLine",
+				    }, {
+				        "valueAxis": "a2a2",
+				        "lineColor": "#FF3399",
+				        "bullet": "triangleRight",
+				        "bulletBorderThickness": 1,
+				        "hideBulletsCount": 30,
+				        "title": "A2A2",
+				        "valueField": "A2A2",
+						"fillAlphas": 0,
+				        "type": "smoothedLine",
+				    },];
+			break;
+		case "pop_count":
+			return "Pop Count";
+		case "genotypes":
+			return "Genotypes";
+		case "phenotypes":
+			return "Phenotypes";
+		default:
+			return "";
+	}
+
+	chartdata.data = cdata;
+	chartdata.title = title;
+	chartdata.valueAxes = valueAxes;
+	chartdata.graphs = graphs;
+
+	return chartdata;
+}
+
+function getChart(name)
+{
+	var chartData = getChartData(name);
+
+	var chart = AmCharts.makeChart("chartdiv", {
+		"titles": 
+		[
+			{
+				"text": chartData.title,
+				"size": 15
+			}
+		],
+	    "type": "serial",
+	    "theme": "none",
+	    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+	    "legend": {
+	        "useGraphSettings": true
+	    },
+	    "dataProvider": chartData.data,
+	    "valueAxes": chartData.valueAxes,
+	    "graphs": chartData.graphs,
+	    "chartScrollbar": {},
+	    "chartCursor": {
+	        "cursorPosition": "mouse"
+	    },
+	    "categoryField": "count",
+	    "categoryAxis": {
+	        //"parseDates": true,
+	        "axisColor": "#DADADA",
+	        "minorGridEnabled": true
+	    },
+	});
+
+	chart.addListener("dataUpdated", zoomChart);
+	zoomChart(chart);
+}
+
+function zoomChart(chart)
+{
+    chart.zoomToIndexes(chart.dataProvider.length - 20, chart.dataProvider.length - 1);
+}
+
+function getTitle(data,data1)
+{
+	var ret = "";
+	$.each(basics.data[basics.count-1], function(key,val){
+		ret += key+": " + val + "<br/>";
+	});
+	return ret;
+}
---
+++ custom_libs/customjs_drift.js
@@ -0,0 +1,119 @@
+
+function init()
+{
+	var obj = {};
+	obj.count = 0;
+	obj.data = [];
+	var temp = {};
+	temp.populationSize = $("#population").val();
+	//temp.dominant = $("#edit-submitted-dominance").val();
+	if($("#dominance").val() == "RED")
+		temp.dominant = "red";
+	else
+		temp.dominant = "blue";
+	temp.startFrequencyA1 = $("#freq").val();
+	temp.startFrequencyA2 = 1 - temp.startFrequencyA1;
+	temp.currentFrequencyA1 = $("#freq").val();
+	temp.currentFrequencyA2 = 1 - temp.startFrequencyA1;
+	obj.data[obj.count] = temp;
+	return obj;
+}
+
+function zeroGeneration()
+{
+	var temp = basics.data[basics.count];
+	temp.currentFrequencyA1A1 = temp.currentFrequencyA1*temp.currentFrequencyA1;
+	temp.currentFrequencyA1A2 = 2*temp.currentFrequencyA1*temp.currentFrequencyA2;
+	temp.currentFrequencyA2A2 = temp.currentFrequencyA2*temp.currentFrequencyA2;
+	basics.data[basics.count] = temp;
+	
+	calculateAGeneration();
+	basics.count++;
+}
+
+function calculateAGeneration()
+{
+	var tempobj = basics.data[basics.count];
+	var temp = 0.0;
+	if(tempobj.dominant == "red")
+	{
+		temp = tempobj.populationSize * tempobj.currentFrequencyA2A2;
+		tempobj.currentHomozygousRecessiveCount = Math.round(temp);
+	
+		temp = tempobj.populationSize * tempobj.currentFrequencyA1A1;
+		tempobj.currentHomozygousDominantCount = Math.round(temp);
+	}
+	
+	if(tempobj.dominant == "blue"){
+		// Calculate the N(a1a1) count as a temporary variable 
+		temp = tempobj.populationSize * tempobj.currentFrequencyA1A1;
+		
+		tempobj.currentHomozygousRecessiveCount = Math.round(temp);
+		
+		// Calculate the N(a2a2) count as a temporary variable
+		temp = tempobj.populationSize * tempobj.currentFrequencyA2A2;
+		tempobj.currentHomozygousDominantCount = Math.round(temp);
+	}
+	// Calculate the N(a1a2) as the remainder of the population size.
+	tempobj.currentHeterozygousCount = (tempobj.populationSize - (tempobj.currentHomozygousDominantCount + tempobj.currentHomozygousRecessiveCount));
+	tempobj.currentDominantPopCount = tempobj.currentHeterozygousCount + tempobj.currentHomozygousDominantCount;
+	tempobj.currentRecessivePopCount = tempobj.currentHomozygousRecessiveCount;
+	basics.data[basics.count] = tempobj;
+}
+
+function calculateAlleleFrequencies()
+{
+	var temp = basics.data[basics.count];
+
+//DRIFT custom
+	var currentfA1 = temp.currentFrequencyA1;
+	
+	if(currentfA1 == 0.0)
+	{					
+		temp.currentFrequencyA1 = 0.0;
+		temp.currentFrequencyA2 = 1.0;
+	}
+	else if( currentfA1 == 1.0)
+	{	
+		temp.currentFrequencyA1 = 1.0;
+		temp.currentFrequencyA2 = 0.0;
+		
+	}
+	else
+	{
+		var twicePop = 2*temp.populationSize;
+		var a1alleleCount = 0;
+		for(var i = 0; i < twicePop; i++)
+		{
+			if(Math.random() < currentfA1)
+			{
+				a1alleleCount++;
+			}
+		}
+
+		var tmp = a1alleleCount/twicePop;
+		temp.currentFrequencyA1 = tmp;
+		temp.currentFrequencyA2 = 1 - tmp;
+	}
+//
+
+
+	temp.currentFrequencyA1A1 = temp.currentFrequencyA1*temp.currentFrequencyA1;
+	temp.currentFrequencyA1A2 = 2*temp.currentFrequencyA1*temp.currentFrequencyA2;
+	temp.currentFrequencyA2A2 = temp.currentFrequencyA2*temp.currentFrequencyA2;
+	basics.data[basics.count] = temp;
+}
+
+function calculateNextGeneration()
+{
+	basics.data[basics.count] = jQuery.extend(true, {}, basics.data[basics.count-1]);
+	calculateAlleleFrequencies();
+	calculateAGeneration();
+	basics.count++;
+}
+
+function calculateNextNGenerations(generations)
+{
+	for(var i = 0; i < generations; i++)
+		calculateNextGeneration();
+}
